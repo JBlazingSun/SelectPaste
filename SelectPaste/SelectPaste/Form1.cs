89190b93b84573c15c0207588937895e0a560e02
blazings <328801898@qq.com>
2017-09-03 23:31:08 +0800

    initial

---
+++ SelectPaste/SelectPaste/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SelectPaste
+{
+    public partial class SelectPaste_From : Form
+    {
+        public SelectPaste_From()
+        {
+            InitializeComponent();
+        }
+    }
+}
